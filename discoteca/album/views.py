f343fdf714b19d35689a55ce971034ba65070aca
jandersong4 <jandersong4@ufmg.br>
2021-01-08 18:30:29 -0300

    Rotas

--- discoteca/album/views.py
+++ discoteca/album/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 album = Blueprint('album',__name__, template_folder='templates')
 
 @album.route('/')
 def index():
-    return "Album"
+  return render_template('albuns.html')
