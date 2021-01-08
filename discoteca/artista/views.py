56245bf18a1f94ba15234be84abc5d091be51bf3
jandersong4 <jandersong4@ufmg.br>
2021-01-08 18:27:33 -0300

    Rotas

--- discoteca/artista/views.py
+++ discoteca/artista/views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 artista = Blueprint('artista',__name__, template_folder='templates')
 
 @artista.route('/')
 def index():
-    return "Artista"
+    return render_template('artistas.html')
+
+@artista.route('/cadastrar',methods=["GET","POST"])
+def cadastrar():
+    return render_template('cadastrar_artistas.html')     
