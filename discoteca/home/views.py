c93def33355bb38245b6812643df0918bca3df9b
Bernardo Borges <bernborgess@outlook.com>
2021-01-09 11:39:07 -0300

    Home com Banco de Dados

--- discoteca/home/views.py
+++ discoteca/home/views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from discoteca import db
+from discoteca.artista.models import Artista
+from discoteca.album.models import Album
 
 home = Blueprint('home',__name__)
 
 @home.route('/')
 def index():
-    return render_template('index.html')
+    artistas = Artista.query.all()
+    albuns = Album.query.all()
+    return render_template('index.html', albuns=albuns, artistas=artistas)
