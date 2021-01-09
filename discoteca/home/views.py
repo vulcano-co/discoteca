8edddf1e9d15113884535e0fe9f39ac45be5b5c3
Bernardo Borges <bernborgess@outlook.com>
2021-01-09 12:00:09 -0300

    Como usuário, na Página Inicial, eu quero poder ver os últimos 5 álbuns cadastrados;

--- discoteca/home/views.py
+++ discoteca/home/views.py
@@ -2,11 +2,12 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from discoteca import db
 from discoteca.artista.models import Artista
 from discoteca.album.models import Album
+from sqlalchemy import desc
 
 home = Blueprint('home',__name__)
 
 @home.route('/')
 def index():
     artistas = Artista.query.all()
-    albuns = Album.query.all()
+    albuns = Album.query.order_by(Album.id.desc()).limit(5)
     return render_template('index.html', albuns=albuns, artistas=artistas)
