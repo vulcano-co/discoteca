782572142a69ca59cedc48564ff47e1682cdf754
Bernardo Borges <bernborgess@outlook.com>
2021-01-09 00:33:47 -0300

    Banco de Dados no front end (albuns)

---
+++ discoteca/album/models.py
@@ -0,0 +1,15 @@
+from discoteca import app, db
+
+
+class Album(db.Model):
+    __tablename__ = "album"
+
+    id              = db.Column(db.Integer, primary_key=True)
+    nome            = db.Column(db.String(30), nullable=False)
+    ano             = db.Column(db.Integer, nullable=False)
+    img             = db.Column(db.String(2083), nullable=False)
+    duracao         = db.Column(db.Integer)
+    genero_musical  = db.Column(db.String(30), nullable=False)
+    idioma          = db.Column(db.String(30), nullable=False)
+    formato         = db.Column(db.String(30), nullable=False)
+    avaliacao       = db.Column(db.Integer, nullable=False)
--- discoteca/album/views.py
+++ discoteca/album/views.py
@@ -1,7 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from discoteca import db
+from discoteca.album.models import Album
 
 album = Blueprint('album',__name__, template_folder='templates')
 
 @album.route('/')
 def index():
-  return render_template('albuns.html')
+  albuns = Album.query.all()
+  return render_template('album.html', albuns=albuns)
+
+
+
+@album.route('/cadastrar',methods=['GET','POST'])
+def cadastrar():
+    if request.method == 'POST':
+        nome            = request.form['nome']
+        ano             = request.form['ano']
+        img             = request.form['img']
+        duracao         = request.form['duracao']
+        genero_musical  = request.form['genero_musical']
+        idioma          = request.form['idioma']
+        formato         = request.form['formato']
+        avaliacao       = request.form['avaliacao']
+
+        album = Album(nome=nome,
+                      ano=ano,
+                      img=img,
+                      duracao=duracao,
+                      genero_musical=genero_musical,
+                      idioma=idioma,
+                      formato=formato,
+                      avaliacao=avaliacao)
+        db.session.add(album)
+        db.session.commit()
+
+        return redirect(url_for('album.index'))
+
+
+    return render_template('cadastrar_album.html')     
