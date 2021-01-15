5c09b50b0c1fb1876b1965b67b685e6504b821cd
luca1-1 <71565597+luca1-1@users.noreply.github.com>
2021-01-15 14:27:22 -0300

    Acerto de erros na responsividade, limpeza de arquivos não utilizados

--- discoteca/__init__.py
+++ discoteca/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-print(__name__)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "qualquercoisa"
